15fd5ad7746573ff2001a9ad9a7eea1f38597b4b
SabihSK <sabih.sk1@gmail.com>
2023-08-02 10:03:55 +0500

    update kafka producer

--- kafka_producer.py
+++ kafka_producer.py
@@ -3,13 +3,13 @@ from kafka import KafkaProducer
 from time import sleep
 import pandas as pd
 
-print("making consumer")
+print("making producer")
 producer = KafkaProducer(bootstrap_servers=['3.83.128.220:9092'],
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 data = pd.read_csv('data/indexProcessed.csv')
 
-print("sending to consumer")
+print("sending to producer")
 while True:
     msg = data.sample(1).to_dict(orient="records")[0]
     producer.send('demo_testing2', value=msg)
